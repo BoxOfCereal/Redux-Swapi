5a09f0f8946a425509031ccb90d2a64c9ea36e07
Noah <noah.stoeckel@gmail.com>
2019-02-05 11:19:36 -0500

    Implements Actions in starWarsReducer.js
    - Also fixes (lack of) exports inside actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-const FETCHING = "FETCHING";
-const SUCCESS = "SUCCESS";
-const FAILURE = "FAILURE";
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
